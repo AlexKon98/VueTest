d7ddd2dc3ab171f86e834d07acaabb3d136202e5
Aleksander <aleksander.kononenko2016@yandex.ru>
2022-10-11 21:31:43 +0300

    fixed ProductItem label's for

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -16,25 +16,25 @@
 
       <ul class="colors colors--black">
         <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only"
+          <label class="colors__label" :for="product.id+'_'+'blue'">
+            <input class="colors__radio sr-only" :id="product.id+'_'+'blue'"
             type="radio" name="color-1"
-            value="#73B6EA" checked="">
+            value="#73B6EA">
             <span class="colors__value" style="background-color: #73B6EA;">
             </span>
           </label>
         </li>
         <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only"
+          <label class="colors__label" :for="product.id+'_'+'green'">
+            <input class="colors__radio sr-only" :id="product.id+'_'+'green'"
             type="radio" name="color-1" value="#8BE000">
             <span class="colors__value" style="background-color: #8BE000;">
             </span>
           </label>
         </li>
         <li class="colors__item">
-          <label class="colors__label">
-            <input class="colors__radio sr-only"
+          <label class="colors__label" :for="product.id+'_'+'black'">
+            <input class="colors__radio sr-only" :id="product.id+'_'+'black'"
             type="radio" name="color-1" value="#222">
             <span class="colors__value" style="background-color: #222;">
             </span>
