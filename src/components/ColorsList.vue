035d8ae2d653eec9e265f3018bc0fc67161a77b0
Aleksander <aleksander.kononenko2016@yandex.ru>
2022-11-14 16:57:22 +0300

    fixed ColorList keys

--- src/components/ColorsList.vue
+++ src/components/ColorsList.vue
@@ -4,7 +4,7 @@
 
     <li class="colors__item">
       <label class="colors__label" :for="product.id+'_'+'blue'">
-        <input class="colors__radio sr-only" :id="product.id+'_'+'blue'" type="radio" :value="product.colors[0]" v-model="color" name="color">
+        <input class="colors__radio sr-only" :id="product.id+'_'+'blue'" type="radio" :value="product.colors[0]" v-model="color" :name="'color'+product.id" checked="">
         <span class="colors__value" :style="{'background-color': product.colors[0]}" :class="{'active': product.colors[0] === '#FFF'}">
         </span>
       </label>
@@ -13,7 +13,7 @@
     <li class="colors__item">
       <label class="colors__label"
       :for="product.id+'_'+'green'">
-        <input class="colors__radio sr-only" :id="product.id+'_'+'green'" type="radio" :value="product.colors[1]" v-model="color" name="color">
+        <input class="colors__radio sr-only" :id="product.id+'_'+'green'" type="radio" :value="product.colors[1]" v-model="color" :name="'color'+product.id">
         <span class="colors__value" :style="{'background-color': product.colors[1]}" :class="{'active': product.colors[1] === '#FFF'}">
         </span>
       </label>
@@ -21,7 +21,7 @@
 
     <li class="colors__item">
       <label class="colors__label" :for="product.id+'_'+'black'">
-        <input class="colors__radio sr-only" :id="product.id+'_'+'black'" type="radio" :value="product.colors[2]" v-model="color" name="color">
+        <input class="colors__radio sr-only" :id="product.id+'_'+'black'" type="radio" :value="product.colors[2]" v-model="color" :name="'color'+product.id">
         <span class="colors__value" :style="{'background-color': product.colors[2]}" :class="{'active': product.colors[2] === '#FFF'}">
         </span>
       </label>
